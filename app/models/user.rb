77c1d4f12562052d2bd1b85cfdd47c9f1396938d
urotansh <114655455+urotansh@users.noreply.github.com>
2022-11-10 06:23:02 +0900

    [Add] User/GroupUser/Groupモデルのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :group_users
+  has_many :groups, through: :group_users
+  
   # userとrelationshipsが1:Nの関係
   has_many :relationships, class_name: "Relationship",
                            foreign_key: "follower_id",
